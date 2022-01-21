02f0f1ffad1e6fdda24c0da0323c90d852e11970
zarychl <48286300+zarychl@users.noreply.github.com>
2022-01-21 14:46:19 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 include_once("settings.php");//plik ze stałymi do połączenia z bazą
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);//tworzenie połączenia z bazą
 
-// sprawdzamy czy nie ma błęu połączenia
+// sprawdzamy czy nie ma błędu połączenia
 if ($mysqli -> connect_errno) {
   echo "Błąd łączenia z badą danych: " . $mysqli -> connect_error;
   exit();
